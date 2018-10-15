4625632ca5bc18d8ece54a750791fdbd79c00baa
fernando.martins <fernando.martins@meta3.com.br>
2018-10-15 08:07:13 -0300

    classe person

--- RestWithASP-NET 02 - Persons/RestWithASP-NET/Controllers/PersonsController.cs
+++ RestWithASP-NET 02 - Persons/RestWithASP-NET/Controllers/PersonsController.cs
@@ -8,7 +8,7 @@ namespace RestWithASP_NET.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class ValuesController : ControllerBase
+    public class PersonsController : ControllerBase
     {
         // GET api/values
         [HttpGet]
